2d14f1a83a3869577c82d9d99e7b5d261c53e6b0
Mukherjee_a <Mukherjee_a@Arunava-MJ.India.XoriantCorp.com>
2018-11-09 10:50:29 +0530

    TCs for frame

--- Object Repository/Internet_herokuapp/Header.rs
+++ Object Repository/Internet_herokuapp/Header.rs
@@ -1,79 +1,73 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>a_some-file.txt</name>
+   <name>Header</name>
    <tag></tag>
-   <elementGuidId>53cd9e2d-de7d-40ae-9038-6684054e7958</elementGuidId>
-   <selectorMethod>XPATH</selectorMethod>
+   <elementGuidId>5830196e-54c7-4899-a152-d3b9ea12e2d5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//h3[(text() = 'An iFrame containing the TinyMCE WYSIWYG Editor' or . = 'An iFrame containing the TinyMCE WYSIWYG Editor')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='content']/div/h3</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>tag</name>
       <type>Main</type>
-      <value>a</value>
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>true</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>href</name>
-      <type>Main</type>
-      <value>download/some-file.txt</value>
+      <value>h3</value>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>text</name>
       <type>Main</type>
-      <value>some-file.txt</value>
+      <value>An iFrame containing the TinyMCE WYSIWYG Editor</value>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>id(&quot;content&quot;)/div[@class=&quot;example&quot;]/a[2]</value>
+      <value>id(&quot;content&quot;)/div[@class=&quot;example&quot;]/h3[1]</value>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:idRelative</name>
-      <value>//div[@id='content']/div/a[2]</value>
-   </webElementXpaths>
-   <webElementXpaths>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:link</name>
-      <value>//a[contains(text(),'some-file.txt')]</value>
+      <value>//div[@id='content']/div/h3</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='File Downloader'])[1]/following::a[2]</value>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='File'])[1]/preceding::h3[1]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='ObjectivityTestAutomationCSHarpFramework.txt'])[1]/preceding::a[2]</value>
-   </webElementXpaths>
-   <webElementXpaths>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:neighbor</name>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='selenium-java-3.141.0.jar'])[1]/preceding::a[3]</value>
-   </webElementXpaths>
-   <webElementXpaths>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:href</name>
-      <value>//a[contains(@href, 'download/some-file.txt')]</value>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Edit'])[1]/preceding::h3[1]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:position</name>
-      <value>//a[2]</value>
+      <value>//h3</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <value></value>
    </webElementXpaths>
 </WebElementEntity>
--- Scripts/Arunava/TC2_Frames/Script1541739684214.groovy
+++ Scripts/Arunava/TC2_Frames/Script1541739684214.groovy
@@ -2,10 +2,13 @@ import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import org.junit.After as After
+
+import javax.xml.soap.Text
+
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.mobile.keyword.builtin.VerifyElementTextKeyword
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
@@ -14,29 +17,8 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-// WebSite Url
-	WebUI.openBrowser('https://the-internet.herokuapp.com/download')
-
-// Maximizing Window
-	WebUI.maximizeWindow()
-
-// Clicking on File to Download 
-	WebUI.click(findTestObject('null'))
-
-// Verifying Downloaded File 
-	String downloadPath = 'C:\\Users\\mojidra_n\\Downloads'
-	String fileName = '1.jpg' 
-
-	Boolean isFileDownloaded(String downloadPath, String fileName) {
-    File dir = new File(downloadPath)
-
-    File[] dirContents = dir.listFiles()
-
-    for (int i = 0; i < dirContents.length; i++) {
-        if (dirContents[i].getName().equals(fileName)) {
-            return true
-        }
-    }
-    
-    return false
-}
+WebUI.openBrowser('http://the-internet.herokuapp.com/iframe')
+String s=WebUI.getText(findTestObject('Object Repository/Internet_herokuapp/Header'))
+println s
+WebUI.verifyElementText(findTestObject('Object Repository/Internet_herokuapp/Header'),'An iFrame containing the TinyMCE WYSIWYG Editor' )
+//WebUI.verifyElementAttributeValue(findTestObject('Object Repository/Internet_herokuapp/Header'),'tag','h3',10)
