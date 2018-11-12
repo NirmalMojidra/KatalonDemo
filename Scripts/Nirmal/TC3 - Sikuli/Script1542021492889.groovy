fd2570e1251a490b9bf974d8dda0a55edb9c3df9
mojidra_n <mojidra_n@Nirmal-M.India.XoriantCorp.com>
2018-11-12 17:09:03 +0530

    jira integration

--- Scripts/Nirmal/TC3 - Sikuli/Script1542021492889.groovy
+++ Scripts/Nirmal/TC3 - Sikuli/Script1542021492889.groovy
@@ -2,7 +2,6 @@ import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import org.junit.After as After
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
@@ -14,29 +13,3 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-// WebSite Url
-	WebUI.openBrowser('https://the-internet.herokuapp.com/download')
-
-// Maximizing Window
-	WebUI.maximizeWindow()
-
-// Clicking on File to Download 
-	WebUI.click(findTestObject('null'))
-
-// Verifying Downloaded File 
-	String downloadPath = 'C:\\Users\\mojidra_n\\Downloads'
-	String fileName = '1.jpg' 
-
-	Boolean isFileDownloaded(String downloadPath, String fileName) {
-    File dir = new File(downloadPath)
-
-    File[] dirContents = dir.listFiles()
-
-    for (int i = 0; i < dirContents.length; i++) {
-        if (dirContents[i].getName().equals(fileName)) {
-            return true
-        }
-    }
-    
-    return false
-}
