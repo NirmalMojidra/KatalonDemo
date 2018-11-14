8a7b4e1dd91629347903dbd3ab366a5bc7396e84
bhanawat_m <bhanawat_m@Digambar-SK.India.XoriantCorp.com>
2018-11-14 15:55:51 +0530

    .

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-11-14T15:46:29</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c263f2b0-b6e8-495a-a6ce-c7977d1a73ad</testSuiteGuid>
+   <testCaseLink>
+      <guid>8284181b-6f8f-42a6-9107-afc99cb6c065</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mohit/New Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
