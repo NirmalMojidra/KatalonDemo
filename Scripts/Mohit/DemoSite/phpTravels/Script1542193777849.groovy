e66506c9c3c48e05aa0c47535382bea53f880aac
bhanawat_m <bhanawat_m@Digambar-SK.India.XoriantCorp.com>
2018-11-15 16:39:04 +0530

    .

--- Scripts/Mohit/DemoSite/phpTravels/Script1542193777849.groovy
+++ Scripts/Mohit/DemoSite/phpTravels/Script1542193777849.groovy
@@ -3,10 +3,21 @@ import org.openqa.selenium.Point as Point
 import org.openqa.selenium.WebDriver as WebDriver
 import org.openqa.selenium.WebElement as WebElement
 import org.openqa.selenium.interactions.Actions as Actions
-
-import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import internal.GlobalVariable as GlobalVariable
 
 WebUI.openBrowser(null, FailureHandling.CONTINUE_ON_FAILURE)
 
@@ -14,8 +25,7 @@ WebUI.maximizeWindow()
 
 WebDriver driver = DriverFactory.getWebDriver()
 
-driver.get("https://www.phptravels.net/")
-
+driver.get('https://www.phptravels.net/')
 
 //Actions action = new Actions(driver)
 for (int i = 0; i < 1000; i++) {
@@ -64,9 +74,10 @@ for (WebElement webElement : elements) {
     if ((loc.getX() == 0) || (loc.getY() == 0)) {
         continue
     }
-	if(webElement.getText().equals("Enter City Or Airport") == false){
-		continue
-	}
+    
+    if (webElement.getText().equals('Enter City Or Airport') == false) {
+        continue
+    }
     
     action.moveToElement(webElement).click().perform()
 
@@ -78,3 +89,91 @@ for (WebElement webElement : elements) {
 
     driver.findElements(By.className('select2-result')).get(0).click()
 }
+
+    driver.findElement(By.name('departure')).click()
+
+    Thread.sleep(1000)
+
+    elements = driver.findElements(By.className('day'))
+
+    for (WebElement webElement : elements) {
+        if (webElement.getText().equals('18')) {
+            webElement.click()
+
+            Thread.sleep(1000)
+
+            break
+        }
+    }
+    
+    elements = driver.findElements(By.className('pfb0'))
+
+    for (WebElement webElement : elements) {
+        System.out.println(webElement.getText())
+
+        if (webElement.getText().equals('SEARCH')) {
+            webElement.click()
+        }
+    }
+    
+    for (int i = 0; i < 1000; i++) {
+        try {
+            driver.findElement(By.id('bookbtn')).isDisplayed()
+
+            Thread.sleep(1000)
+
+            break
+        }
+        catch (Exception e) {
+            Thread.sleep(1000)
+        } 
+    }
+    
+    driver.findElements(By.id('bookbtn')).get(0).click()
+
+    for (int i = 0; i < 1000; i++) {
+        try {
+            driver.findElement(By.name('firstname')).isDisplayed()
+
+            Thread.sleep(1000)
+
+            break
+        }
+        catch (Exception e) {
+            Thread.sleep(1000)
+        } 
+    }
+    
+    driver.findElement(By.name('firstname')).sendKeys('Firstname')
+
+    driver.findElement(By.name('lastname')).sendKeys('LastName')
+
+    driver.findElement(By.name('email')).sendKeys('dummyemail@email.com')
+
+    driver.findElement(By.name('confirmemail')).sendKeys('dummyemail@email.com')
+
+    driver.findElement(By.name('phone')).sendKeys('9874563201')
+
+    driver.findElement(By.name('address')).sendKeys('Dummy Address')
+	
+	elements = driver.findElements(By.className("btn-success"));
+	for (WebElement webElement : elements) {
+		if(webElement.getText().equals("CONFIRM THIS BOOKING")) {
+			webElement.click();
+		}
+	}
+	
+	for (int i = 0; i < 1000; i++) {
+		try {
+			driver.findElement(By.className("arrivalpay")).isDisplayed();
+			Thread.sleep(1000);
+			break;
+		} catch (Exception e) {
+			Thread.sleep(1000);
+		}
+	}
+	
+	driver.findElement(By.className("arrivalpay")).click();
+	Thread.sleep(1000);
+	driver.switchTo().alert().accept();
+
--- Test Suites/New Test Suite.ts
+++ Test Suites/New Test Suite.ts
@@ -4,7 +4,7 @@
    <name>New Test Suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-11-15T12:31:18</lastRun>
+   <lastRun>2018-11-15T15:14:35</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
@@ -12,9 +12,9 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>c263f2b0-b6e8-495a-a6ce-c7977d1a73ad</testSuiteGuid>
    <testCaseLink>
-      <guid>28cd1beb-29d5-46f2-9457-7e439cefa4b1</guid>
+      <guid>1522a0b6-eec0-429b-8942-c17fb43b161b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Mohit/DemoSite/phpTravels</testCaseId>
+      <testCaseId>Test Cases/Mohit/Drag_Drop</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
