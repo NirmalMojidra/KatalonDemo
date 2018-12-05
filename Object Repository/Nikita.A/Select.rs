4105570d847fa37d6347335cce359e88a764180b
Agrawal_nkt <Agrawal_nkt@Nikita-AL.India.XoriantCorp.com>
2018-12-05 17:42:01 +0530

    Dropdown handling
    Change-Id: I5abb7d6e33a673b52f03ab48dfed7f6089df5aa0
    Signed-off-by: Agrawal_nkt <Agrawal_nkt@Nikita-AL.India.XoriantCorp.com>

---
+++ Object Repository/Nikita.A/Select.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Select</name>
+   <tag></tag>
+   <elementGuidId>b5203597-8d86-4216-8bb8-51d7fd934b12</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'select-demo']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>select-demo</value>
+   </webElementProperties>
+</WebElementEntity>
