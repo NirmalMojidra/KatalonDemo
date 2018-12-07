5af01a8ff211fdf03f6cd904da99737709e04ca2
Gupta_shr <Gupta_shr@Shrutika-GT.India.XoriantCorp.com>
2018-12-07 19:02:39 +0530

    Iframes and Alerts code
    Change-Id: I3db775f823017b1c13a34daa6eb2d29b57fcf1bc
    Signed-off-by: Gupta_shr <Gupta_shr@Shrutika-GT.India.XoriantCorp.com>

---
+++ Object Repository/Page_TOOLSQA  Free QA Automation To/input_First name_firstname.rs
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_First name_firstname</name>
+   <tag></tag>
+   <elementGuidId>492a376c-64eb-40c0-af5d-28f302b437d0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name='firstname']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//input[@name=&quot;firstname&quot;][count(. | //*[@ref_element = 'Object Repository/Page_TOOLSQA  Free QA Automation To/iframe_1']) = count(//*[@ref_element = 'Object Repository/Page_TOOLSQA  Free QA Automation To/iframe_1'])]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//input[@name=&quot;firstname&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <value>//input[@name='firstname']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <value>//div[@id='content']/div/div/div/div/div[2]/div/form/fieldset/div[8]/input</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='First name:'])[1]/following::input[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Link Test'])[1]/following::input[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Last name:'])[1]/preceding::input[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <value>//div[8]/input</value>
+   </webElementXpaths>
+</WebElementEntity>
