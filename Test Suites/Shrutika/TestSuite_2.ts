685c998c12bea9d0aca3d857a182d57a905489fd
Gupta_shr <Gupta_shr@Shrutika-GT.India.XoriantCorp.com>
2018-12-12 15:56:46 +0530

    Katalon Analytics Configuration
    Change-Id: I9d30a1b3ce936dbd25fa7f72385185c524e010cb
    Signed-off-by: Gupta_shr <Gupta_shr@Shrutika-GT.India.XoriantCorp.com>

---
+++ Test Suites/Shrutika/TestSuite_2.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>TestSuite for Iframe and Popup</description>
+   <name>TestSuite_2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-12-12T15:48:40</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>84f883ec-af9e-4d0c-83e6-0f5aae1ecd42</testSuiteGuid>
+   <testCaseLink>
+      <guid>a3153e6a-a030-4156-866e-6aa0982e970a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Shrutika/IFrame Test Case</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>eed43380-266e-486d-9b47-331256463730</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Shrutika/PopUp/AlertPopUp Test Case</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c52db6b9-5889-439e-a4a8-bf1ba4a068f0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Shrutika/PopUp/AlertWindowPopUp Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
