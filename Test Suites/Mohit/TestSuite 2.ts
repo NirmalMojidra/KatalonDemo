a613eda98e806b9ed84042eabc5596a5ba64fc26
C5281054 <C5281054@INLN56580283A.apj.global.corp.sap>
2018-12-12 11:16:06 +0530

    fix

--- Test Suites/Mohit/TestSuite 2.ts
+++ Test Suites/Mohit/TestSuite 2.ts
@@ -4,7 +4,7 @@
    <name>TestSuite 2</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-12-12T11:11:21</lastRun>
+   <lastRun>2018-12-12T11:13:40</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
