fa15c4c702f119eed5c0b79b61b8eb674558d01c
Agrawal_nkt <Agrawal_nkt@Nikita-AL.India.XoriantCorp.com>
2018-12-12 15:18:10 +0530

    Adding failure handling Change-Id: I389c409c670cf29b7cd98c935f39686e057a6ad8 Signed-off-by: Agrawal_nkt <Agrawal_nkt@Nikita-AL.India.XoriantCorp.com>

--- Scripts/Nikita.A/Basiclogin/Script1544519950043.groovy
+++ Scripts/Nikita.A/Basiclogin/Script1544519950043.groovy
@@ -34,7 +34,11 @@ WebUI.setText(findTestObject('Object Repository/Basiclogin/input_Username_txtPas
 
 WebUI.click(findTestObject('Object Repository/Basiclogin/input_Password_Submit (1)'))
 
-CustomKeywords.'nikitas.a.Sample.checkelement'(findTestObject('Object Repository/Basiclogin/a_Welcome Admin'))
+//CustomKeywords.'nikitas.a.Sample.checkelement'(findTestObject('Object Repository/Basiclogin/a_Welcome Admin'))
+
+Text = WebUI.getText(findTestObject('Object Repository/Basiclogin/span_Assign Leave'))
+
+WebUI.verifyEqual(Text, "Assign123", FailureHandling.CONTINUE_ON_FAILURE)
 
 
 CustomKeywords.'nikitas.a.Sample.checkelement'(findTestObject('Object Repository/Basiclogin/img'))
