44d8f4b677954b13aa3653c056af1c22ad9ff814
C5281054 <C5281054@INLN56580283A.apj.global.corp.sap>
2018-12-13 16:39:00 +0530

    .

--- Test Suites/Shrutika/alerts_and_popup.ts
+++ Test Suites/Shrutika/alerts_and_popup.ts
@@ -1,30 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
-   <description>TestSuite for Iframe and Popup</description>
-   <name>TestSuite_2</name>
+   <description></description>
+   <name>alerts_and_popup</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-12-13T15:03:14</lastRun>
+   <lastRun>2018-12-13T16:21:15</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <testSuiteGuid>84f883ec-af9e-4d0c-83e6-0f5aae1ecd42</testSuiteGuid>
+   <testSuiteGuid>a0a6e1bf-18eb-4e01-b783-b70f51420d59</testSuiteGuid>
    <testCaseLink>
-      <guid>a3153e6a-a030-4156-866e-6aa0982e970a</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Shrutika/IFrame Test Case</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>eed43380-266e-486d-9b47-331256463730</guid>
+      <guid>1f090f44-cd77-4fcb-9eba-eb9782478d36</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Shrutika/PopUp/AlertPopUp Test Case</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>c52db6b9-5889-439e-a4a8-bf1ba4a068f0</guid>
+      <guid>1405f625-9c87-445f-8e3a-10207aa53256</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Shrutika/PopUp/AlertWindowPopUp Test Case</testCaseId>
