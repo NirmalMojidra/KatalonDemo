da1fae8ec705559b47585535c3a9e29e9c06d335
C5281054 <C5281054@INLN56580283A.apj.global.corp.sap>
2018-12-13 12:32:20 +0530

    .

--- Test Suites/Demo_Collection.ts
+++ Test Suites/Demo_Collection.ts
@@ -3,25 +3,12 @@
    <description></description>
    <name>Demo_Collection</name>
    <tag></tag>
-   <executionMode>PARALLEL</executionMode>
+   <executionMode>SEQUENTIAL</executionMode>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
          <configuration>
             <groupName>Web Desktop</groupName>
             <profileName>default</profileName>
-            <runConfigurationId>Chrome</runConfigurationId>
-         </configuration>
-         <runEnabled>true</runEnabled>
-<<<<<<< HEAD
-         <testSuiteEntity>Test Suites/Mohit/TestSuite 1</testSuiteEntity>
-=======
-         <testSuiteEntity>Test Suites/Mohit/TestSuite_1</testSuiteEntity>
->>>>>>> branch 'master' of https://github.com/NirmalMojidra/KatalonDemo.git
-      </TestSuiteRunConfiguration>
-      <TestSuiteRunConfiguration>
-         <configuration>
-            <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
@@ -34,16 +21,7 @@
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/Thalesh/suite1</testSuiteEntity>
-      </TestSuiteRunConfiguration>
-      <TestSuiteRunConfiguration>
-         <configuration>
-            <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
-            <runConfigurationId>Firefox</runConfigurationId>
-         </configuration>
-         <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/Thalesh/suite1</testSuiteEntity>
+         <testSuiteEntity>Test Suites/Mohit/TestSuite_1</testSuiteEntity>
       </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
