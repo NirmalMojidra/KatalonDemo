ad3e6b7f0df249ce8bb599d652c52abaa057a80a
C5281054 <mohit.bhanawat@gmail.com>
2018-12-17 11:26:44 +0530

    .

--- Test Suites/NikitaS.A/Basic_login.ts
+++ Test Suites/NikitaS.A/Basic_login.ts
@@ -4,7 +4,7 @@
    <name>Basic_login</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-12-17T10:49:33</lastRun>
+   <lastRun>2018-12-17T11:11:00</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
