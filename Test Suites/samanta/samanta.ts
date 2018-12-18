17281d20fa20464eabac6a5608660eaae03a4e42
Ksheerasindhu Samanta <samanta_k@India.XoriantCorp.com>
2018-12-18 11:58:22 +0530

    bash commit

---
+++ Test Suites/samanta/samanta.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>samanta</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-12-17T11:24:46</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8eea1cb6-a23a-4546-a398-2a0c03db3383</testSuiteGuid>
+   <testCaseLink>
+      <guid>4c36d836-22b7-4200-b0e0-ee79b6af1caa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Samanta/TC1_PopUpHandling</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2924d824-0f96-4c88-8cb5-2b6e29513ae0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Samanta/TC2_HandlingCheckBox</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5bf197f0-b87a-4073-8f93-b660e69e9084</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Samanta/TC3_HandlingRadioButton</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e17eb82b-679e-49cb-97eb-d83c1a8dcc0b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Samanta/TC4_CheckPointTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b176cab4-385e-461c-a983-4e52389ede22</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Samanta/TC5_Popop</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
